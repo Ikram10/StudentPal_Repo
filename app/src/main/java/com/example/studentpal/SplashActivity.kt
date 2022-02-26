432f88e3fa6096f0d6a27a7926fa7110ac403742
Ikram10 <Ikram.rashid@city.ac.uk>
2022-02-26 13:00:23 +0000

    Initial Commit: Splash Screen Activity created

---
+++ app/src/main/java/com/example/studentpal/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.studentpal
+
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.widget.TextView
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val typeFace: Typeface = Typeface.createFromAsset(assets,"fonts/Font-Nongman.ttf" )
+        val tvappname : TextView = findViewById(R.id.tv_app_name)
+        tvappname.typeface = typeFace
+
+
+    }
+}
